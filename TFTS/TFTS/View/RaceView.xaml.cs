2096fe644440e0694e2f84e6c0a94e151d072c03
loject <29097142+loject@users.noreply.github.com>
2020-06-30 06:08:50 +0700

    add RaceView

---
+++ TFTS/TFTS/View/RaceView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFTS.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TFTS.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RaceView : ContentPage
+    {
+        public RaceView(Race race)
+        {
+            InitializeComponent();
+            BindingContext = race;
+        }
+    }
+}
