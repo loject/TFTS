f92fff1e0507fc8975933579ad659453fb3cc836
loject <29097142+loject@users.noreply.github.com>
2020-06-30 02:29:08 +0700

    add getStringFromTimeSpan

---
+++ TFTS/TFTS/misc/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TFTS
+{
+    public static class Utils
+    {
+        static public string getStringFromTimeSpan(TimeSpan timeSpan)
+        {
+            int hours = timeSpan.Hours;
+            int minutes = timeSpan.Minutes;
+            int seconds = timeSpan.Seconds;
+            int miliseconds = timeSpan.Milliseconds / 100;
+            return $"{hours:00}:{minutes:00}:{seconds:00}.{miliseconds:0}";
+        }
+    }
+}
