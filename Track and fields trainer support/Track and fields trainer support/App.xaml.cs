00a330c4f34041d3ec05d3f2a38704ad1334f7bb
loject <29097142+loject@users.noreply.github.com>
2020-06-29 11:00:22 +0700

    Add project files.

---
+++ Track and fields trainer support/Track and fields trainer support/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Track_and_fields_trainer_support
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
