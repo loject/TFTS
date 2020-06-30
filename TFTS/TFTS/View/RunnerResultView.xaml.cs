64468cee3d6b401f02dc8016d101e347185a9abb
loject <29097142+loject@users.noreply.github.com>
2020-06-30 15:08:13 +0700

    add RaceResultView

---
+++ TFTS/TFTS/View/RunnerResultView.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFTS.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TFTS.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RunnerResultView : ContentPage
+    {
+        public Runner Runner { get; }
+        public string StartTime { get; }
+        public string Distance { get; }
+        public string About { get => $"{Runner.Name}.\nДистанция {Distance} метров.\nСтарт {StartTime}"; }
+        public RunnerResultView(Runner runner, string startTime, string distance)
+        {
+            InitializeComponent();
+            Runner = runner;
+            StartTime = startTime;
+            Distance = distance;
+            BindingContext = this;
+        }
+
+    }
+
+    public class IndexFromListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return -1;
+            try
+            {
+                var index = ((ListView)parameter).ItemsSource.Cast<object>().ToList().IndexOf(value) + 1;
+                return index;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TotalTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) return -1;
+            try
+            {
+                List<Lap> laps = ((ListView)parameter).ItemsSource.Cast<Lap>().ToList();
+                TimeSpan totalTime = TimeSpan.Zero;
+                foreach(Lap lap in laps)
+                {
+                    if (lap.Equals(value))
+                        break;
+                    else
+                        totalTime += lap.Time;
+                }
+                return Utils.getStringFromTimeSpan(totalTime);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
