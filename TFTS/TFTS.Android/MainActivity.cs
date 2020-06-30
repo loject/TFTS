778d3ad50b795fd4df5d0331d5ebac7ba6fba4a6
loject <29097142+loject@users.noreply.github.com>
2020-06-30 15:53:13 +0700

    enable SwipeView flag

--- TFTS/TFTS.Android/MainActivity.cs
+++ TFTS/TFTS.Android/MainActivity.cs
@@ -19,6 +19,7 @@ namespace TFTS.Droid
 
             base.OnCreate(savedInstanceState);
 
+            global::Xamarin.Forms.Forms.SetFlags("SwipeView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
