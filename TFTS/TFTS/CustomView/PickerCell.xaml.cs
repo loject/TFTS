7a15e3da4defdd275def18bdc05f8e54ab5b7f27
loject <29097142+loject@users.noreply.github.com>
2020-08-17 01:21:51 +0700

    move customviews from misc

--- TFTS/TFTS/CustomView/PickerCell.xaml.cs
+++ TFTS/TFTS/CustomView/PickerCell.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
 
-namespace TFTS.misc.CustomView
+namespace TFTS.CustomView
 {
     [ContentProperty("Items")]
     public partial class PickerCell : ViewCell
