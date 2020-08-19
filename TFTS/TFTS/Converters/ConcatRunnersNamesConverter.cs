9d7a1e1a6b6bec5a03beaaa50d7400c4ea862530
loject <29097142+loject@users.noreply.github.com>
2020-08-19 18:53:34 +0700

    add runner names concater converter for history page

---
+++ TFTS/TFTS/Converters/ConcatRunnersNamesConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using TFTS.misc;
+using TFTS.Models;
+using TFTS.ViewModels;
+using Xamarin.Forms;
+
+namespace TFTS.Converters
+{
+    class ConcatRunnersNamesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "";
+            try
+            {
+                var list = value as SortableObservableCollection<RunnerModel>;
+                if (list.Count == 0) return "Без спортсменов?!";
+                return String.Join(", ", list.Select(r => r.Name));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error - " + e.Message);
+            }
+            return "Error";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
