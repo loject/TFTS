99a9f5304f21b83a24d44423f58617faa8995b9f
loject <29097142+loject@users.noreply.github.com>
2020-08-26 23:44:57 +0700

    set up new race plan adding

--- TFTS/TFTS/ViewModels/PlanedRacesPageViewModel.cs
+++ TFTS/TFTS/ViewModels/PlanedRacesPageViewModel.cs
@@ -23,10 +23,25 @@ namespace TFTS.ViewModels
         public PlanedRacesPageViewModel()
         { }
         #region Commands
-        public ICommand AddRaceToPlanCommand { get => new Command<RaceModel>(Race => 
+        public ICommand AddRaceToPlanCommand { get => new Command<RaceModel>(Race =>
         {
-
+            var RaceSetUpVM = new RaceSetUpViewModel();
+            RaceSetUpVM.ActionAfterEditing = RaceSetUpVM =>
+            {
+                var RaceVM = RaceSetUpVM.GetRaceViewModel();
+                App.PlanDatabase.SaveRaceToRacePlan(RaceVM.Race);
+            };
+            var RaceSetUpPage = new RaceSetUpView();
+            RaceSetUpPage.BindingContext = RaceSetUpVM;
+            Application.Current.MainPage.Navigation.PushAsync(RaceSetUpPage);
         }); }
+        public ICommand GoToRacePage
+        {
+            get => new Command(() =>
+            {
+                /* incoming */
+            });
+        }
         public ICommand ClearAllPlansCommand
         {
             get => new Command(() =>
